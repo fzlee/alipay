11ff0b8767f2bbdf2227e75b399a9acadff7243c
Li Fazheng <fzleee@gmail.com>
2016-12-10 11:57:33 +0800

    setup.py modified

--- setup.py
+++ setup.py
@@ -13,21 +13,16 @@ from setuptools import setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
 setup(
     name="python-alipay-sdk",
     version="0.1",
     author="fzlee",
     author_email="fzleee@gmail.com",
-    description="Python SDK for alipay, RSA is the only sign method we support",
+    description="Python SDK for AliPay, RSA is the only sign method we support",
     license="BSD",
     keywords="python sdk alipay",
     url="https://github.com/fzlee/alipay",
     packages=['alipay'],
-    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
