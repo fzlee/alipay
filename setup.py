0e7b5b38b603ded6a2b157a97e58ce6eb8f2536f
Li Fazheng <fzleee@gmail.com>
2016-12-10 12:14:09 +0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,9 @@
     ~~~~~~~~~~
 
 """
-import os
 from setuptools import setup
 
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 setup(
     name="python-alipay-sdk",
     version="0.1",
@@ -27,5 +22,9 @@ setup(
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
 )
