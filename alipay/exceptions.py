5f65d10d1395420b962a9466fe979cb8b76810fc
fzlee <hi@ifconfiger.com>
2019-01-14 19:47:13 +0800

    bug fix for exception details

--- alipay/exceptions.py
+++ alipay/exceptions.py
@@ -5,6 +5,7 @@
     ~~~~~~~~~~
 
 """
+import sys
 
 
 class AliPayException(Exception):
@@ -12,11 +13,24 @@ class AliPayException(Exception):
         self.__code = code
         self.__message = message
 
-    def __repr__(self):
-        return u"AliPayException<code:{}, message:{}>".format(self.__code, self.__message)
 
-    def __unicode__(self):
-        return u"AliPayException<code:{}, message:{}>".format(self.__code, self.__message)
+    if sys.version_info[0] >= 3:
+        def to_unicode(self):
+            return "AliPayException: code:{}, message:{}".format(self.__code, self.__message)
+
+        def __str__(self):
+            return self.to_unicode()
+
+        def __repr__(self):
+            return self.to_unicode()
+    else:
+        def to_unicode(self):
+            return u"AliPayException: code:{}, message:{}".format(self.__code, self.__message.decode("utf8"))
+        def __str__(self):
+            return self.to_unicode().encode('utf8')
+
+        def __repr__(self):
+            return self.to_unicode().encode('utf8')
 
 
 class AliPayValidationError(Exception):
